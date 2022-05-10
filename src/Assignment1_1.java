33fb5c5dff2f63f9652c10c4ec8d136ad9eb5633
Denzelcole <theocole4@gmail.com>
2022-05-10 01:40:39 +0100

    commit

---
+++ src/Assignment1_1.java
@@ -0,0 +1,12 @@
+public class Assignment1_1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Theodore Ononuju");
+        System.out.println("JAVA Web");
+        System.out.println("Imo, Mgbidi");
+        System.out.println("Raspberry Cheesecake");
+    }
+        
+}
+//End of Assignment 1_1
+
+
