ea2b6cc6fc1a5d0660962b1b4b582e68eb546730
Denzelcole <theocole4@gmail.com>
2022-05-19 00:53:11 +0100

    AIMJAVAUNIT1

---
+++ src/Assingment1_2.java
@@ -0,0 +1,9 @@
+public class Assingment1_2 { 
+    public static void main(String[] args) throws Exception {
+        String logo = "     J    A    V     V     A \n" +
+                      "     J   A A    V   V     A A \n" +
+                      "J    J  AAAAA    V V     AAAAA \n" +
+                      " J  J  A     A    V     A     A";
+        System.out.println(logo);
+    }
+}
