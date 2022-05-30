2d05fa796bcbfd8a58ab8c7eb824863135487cb7
Denzelcole <theocole4@gmail.com>
2022-05-31 00:21:07 +0100

    assignment2_1

---
+++ Assignment2_1/Assignment2_1/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit";
+
+        System.out.println(name);
+    }
+}
