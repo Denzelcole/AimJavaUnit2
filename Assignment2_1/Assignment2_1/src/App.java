cb7fc1588b82a17dac23e14fd92ecef2d5c411d6
Denzelcole <theocole4@gmail.com>
2022-06-14 00:40:54 +0100

    Assignment2_2

--- Assignment2_1/Assignment2_1/src/App.java
+++ Assignment2_1/Assignment2_1/src/App.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class App {
+    
+    
     public static void main(String[] args) throws Exception {
-        String name = "This program converts a temperature in degrees Celsius into a temperature in degrees Fahrenheit";
+
+        Scanner celusScanner = new Scanner(System.in);
+        System.out.println("Enter a temperature in degrees Celsius");
+        System.out.print("Input a degree in Fahrenheit: ");
+        
+
+       // String degree = celusScanner.nextLine();
+
+       Scanner input = new Scanner(System.in);
+        //int temperature = celusScanner.nextInt();
+        double fahrenheit = input.nextDouble();
+        double degree = celusScanner.nextDouble();
+        
+        double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
+        System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+
+
+        String name = "This program converts a temperature in degrees Celsius into a "+
+        "temperature in degrees Fahrenheit.";
+
+        String bye = "Goodbye...";
+        
 
         System.out.println(name);
+        System.out.println(degree);
+        System.out.println(bye);
+        celusScanner.close();
+        input.close();
     }
 }
